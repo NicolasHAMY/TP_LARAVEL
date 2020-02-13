eae43587cbadfc3d0b4fbd401f218b28995da120
simonbourrasseau <simon.bourrasseau@imie.fr>
2020-02-13 14:53:53 +0100

    re-commit redirection + 2/3 bug

--- nicodenv/app/Http/Controllers/PagesController.php
+++ nicodenv/app/Http/Controllers/PagesController.php
@@ -14,11 +14,11 @@ class PagesController extends Controller
 
 	public function postForm(Request $request)
 	{
-            Mail::send(['text' => 'PostForm'], ['name' => $request->input('nom'), 'surname' => $request->input('premon'), 'email' => $request->input('email'),  'question' => $request->input('question')], function($message){
+            Mail::send(['text' => 'PostForm'], ['name' => $request->input('name'), 'surname' => $request->input('surname'), 'email' => $request->input('email'),  'question' => $request->input('question')], function($message){
             $message->from('reply@nicodenv.io')->to('contact@nicodenv.io')->subject('Contact');
             });
 
-            Mail::send(['text' => 'SendMessage'], ['surname' => $request->input('premon'), 'question' => $request->input('question')], function($sm) use ($request) {
+            Mail::send(['text' => 'SendMessage'], ['surname' => $request->input('surname'), 'question' => $request->input('question')], function($sm) use ($request) {
             $sm->from('contact@nicodenv.io')->to($request->email)->subject('Question');
             });
             return redirect("/");
