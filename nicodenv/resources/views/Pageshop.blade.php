b651cbea59e46f8b166ec2d8596803e81684a60f
Nicolas Hamy <nhamypro@gmail.com>
2020-03-11 16:09:17 +0100

    modifications mineures | test ajout seeders + début css shop

--- nicodenv/resources/views/Pageshop.blade.php
+++ nicodenv/resources/views/Pageshop.blade.php
@@ -6,10 +6,10 @@
               <div class="row">
                 <div class="col-12">
 					@foreach ($shop as $shop)
-					    <div style="float: left">
-							<div class="text-primary"><strong>{!! $shop->shop_Article_name !!}</strong></div>
-							<div class="text-primary"><strong>{!! $shop->shop_Article_description !!}</strong></div>
-							<div class="text-primary"><strong>{!! $shop->shop_Article_price !!}</strong></div>
+					    <div class="article col-3"style="float: left">
+							<p class="font-blue"><strong>{!! $shop->shop_Article_name !!}</p>
+							<p class="text-primary"><strong>{!! $shop->shop_Article_description !!}</p>
+							<p class="text-primary"><strong>{!! $shop->shop_Article_price !!}</p>
                         </div>
 					@endforeach
 				    </div>
